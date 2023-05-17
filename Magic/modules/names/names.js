406ee3204b8915d62f655ef7ed8f2fde1fdaf6e2
Ricardo-Antunes0 <ricardofantunes@ua.pt>
2023-05-17 11:47:42 +0100

    Reconhecimento facial e display de imagem

--- Magic/modules/names/names.js
+++ Magic/modules/names/names.js
@@ -1,32 +1,57 @@
 Module.register("names", {
 	defaults: {
-	  message: "bla ",
+	  message: "",
+	  defaultImage: "./modules/names/images/unknown.jpg",
+	  imagePath: "./modules/names/images/"
 	},
+	
 	
 	start: function() {
 	  // Send a socket notification to the node_helper to start listening to the Kafka topic
-	  this.sendSocketNotification("START_CONSUMER");
+	  	this.sendSocketNotification("START_CONSUMER");
 	},
   
 	getStyles: function () {
-		return ["PPG_styles.css"];
+		return ["names.css"];
 	},
 	socketNotificationReceived: function (notification, payload) {
-	  if (notification === "MESSAGE_RECEIVED") {
-		this.config.message = "Bem vindo "+payload;
-		this.updateDom();
-	  }
-	  
+		if (notification === "MESSAGE_RECEIVED" && payload !== undefined && payload !== null && payload !== "") {
+			if (payload === "unknown") {
+			  this.config.message = "";
+			  this.config.url = this.config.defaultImage;
+			} else {
+			  this.config.message = "Bem-vindo " + payload;
+			  this.config.url = this.config.imagePath + this.formatPayload(payload) + ".jpg";
+			}  
+		} else {
+			this.config.message = "";
+			this.config.url = null;
+		}
+
+		this.updateDom();	  
 	},
+
+	formatPayload: function (payload) {
+		return payload.toLowerCase().replace(/\s+/g, "_");
+	  },
 	
 	getDom: function () {
-	  	const messageWrapper = document.createElement("div");
+		const wrapper = document.createElement("div");
+		wrapper.className = "name-wrapper";
+
+		const messageElement = document.createElement("div");
+		messageElement.className = "short-message";
 	  	const message = document.createTextNode(this.config.message);
-		
-		messageWrapper.className = "short-message";
-	
-	  	messageWrapper.appendChild(message);
-		return messageWrapper;
+	  	wrapper.appendChild(message);
+
+		if (this.config.url) {
+			const imageElement = document.createElement("img");
+			imageElement.className = "person-image";
+			imageElement.src = this.config.url;
+			wrapper.appendChild(imageElement);
+		  }
+
+		return wrapper;
 	},
   });
   
